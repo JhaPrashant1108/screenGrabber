1f02c973be623ec3d54b038b3f0fc552bc68b684
JhaPrashant1108 <prashant.jha0008@gmail.com>
2020-07-20 15:11:37 +0530

    Initial Commit

---
+++ screenGrabber.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+from datetime import datetime
+
+
+
+user = 'prashantJha'
+
+while True:
+    image = pyautogui.screenshot()
+    datetimeset = str(datetime.now()).split(" ")
+    name = user+'_'+datetime.now().strftime('_%d_%m_%Y_%H_%M_%S')+'.jpeg'
+    image.save(name)
+    time.sleep(300)
